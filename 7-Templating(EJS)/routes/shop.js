319dbd34b38441ee8f2d2cba30298952b3001681
aryan rawther <aryanedu5786@gmail.com>
2022-06-21 12:26:15 +0530

    learn about ejs templating engine

---
+++ 7-Templating(EJS)/routes/shop.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+const rootDir = require("../util/path");
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    products: products,
+    pageTitle: "Shop",
+    path: "/",
+  });
+});
+
+module.exports = router;
