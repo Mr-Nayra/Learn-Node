62f007e8a77dec58448d3c109ed3601031fadccc
aryan rawther <aryanedu5786@gmail.com>
2022-06-21 20:30:14 +0530

    seperating the app according to model view controller

---
+++ 8-Model View Controller/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.set("view engine", "ejs"); //sets ejs globally as the templating engine
+app.set("views", "views"); //sets the path for the directory in which templates are kept
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+// used to serve static files in public folder like style.css
+
+app.use("/admin", adminRoutes); //routes will start with /admin/..,
+app.use(shopRoutes);
+
+app.use("/", (req, res) => {
+  res.send("<h1>Page not found</h1>");
+});
+
+app.listen(3000);
